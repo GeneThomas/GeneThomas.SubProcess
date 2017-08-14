7f6c63ceae96ff398615ef6bf104f25f88ddb3e1
Gene Thomas <gene@genethomas.com>
2017-08-14 22:28:38 +1200

    readme example cde

---
+++ Gt.SubProcess.UnitTests/ReadmeExample.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.IO;
+using Gt.SubProcess;
+
+namespace ReadmeExample
+{
+    class Program
+    {
+        static int Main(string[] args) { 
+            try {
+                SubProcess.CheckCall("ls", "-l");
+                    // throws if exits with non 0 exit code
+
+                SubProcess p = new SubProcess("ssh", "me@mysite.com")
+                {
+                    Out = new FileStream("ssh-output.txt", FileMode.OpenOrCreate),
+                    Error = SubProcess.Capture,
+                    In = SubProcess.Pipe                    
+                };
+                p.Wait();
+
+                Console.WriteLine(p.ErrorString);
+
+                return 0; 
+            } catch (Exception e) { 
+                Console.Error.WriteLine("Fatal Error: " + e.Message); 
+                return 1; 
+            }
+        } 
+    }
+}
